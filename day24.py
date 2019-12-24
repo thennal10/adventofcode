874b44303af248648e8a40ac6f5d063ec2f80303
Thennal <thennal10@gmail.com>
2019-12-24 12:57:02 +0530

    day24 part2

--- day24.py
+++ day24.py
@@ -1,6 +1,6 @@
 import copy
 
-def check(grid, j, i, direction):
+def check(grids, j, i, direction, depth):
     if direction == 1:
         y, x = j - 1, i
     elif direction == -1:
@@ -10,50 +10,85 @@ def check(grid, j, i, direction):
     elif direction == 2:
         y, x = j, i + 1
 
-    try:
-        if (y >= 0) and (x >= 0) and (grid[y][x] == '#'):
-            return True
-        else:
+    oob = (y < 0) or (y >= len(grid)) or (x < 0) or (x >= len(grid))
+    if oob:
+        d = depth - 1
+        if d < 0:
             return False
-    except IndexError:
-        return False
+        # just the center of the grid
+        c = 2
+        if direction == 1:
+            y, x = c - 1, c
+        elif direction == -1:
+            y, x = c + 1, c
+        elif direction == -2:
+            y, x = c, c - 1
+        elif direction == 2:
+            y, x = c, c + 1
+
+        if grids[d][y][x] == '#':
+            return 1
+        else:
+            return 0
+    elif grids[depth][y][x] == '?':
+        d = depth + 1
+        if d >= len(grids):
+            return 0
+
+        # god help me
+        if direction == 1:
+            row = grids[d][-1]
+            return sum([1 for t in row if t == '#'])
+        elif direction == -1:
+            row = grids[d][0]
+            return sum([1 for t in row if t == '#'])
+        elif direction == -2:
+            column = [row[-1] for row in grids[d]]
+            return sum([1 for t in column if t == '#'])
+        elif direction == 2:
+            column = [row[0] for row in grids[d]]
+            return sum([1 for t in column if t == '#'])
+    else:
+        if grids[depth][y][x] == '#':
+            return 1
+        else:
+            return 0
 
 inp = """#####
 .....
-....#
+..?.#
 #####
 .###."""
 
 grid = []
 for row in inp.split('\n'):
     grid.append([tile for tile in row])
+empty = [['.' for j in range(5)] for i in range(5)]
+empty[2][2] = '?'
+grids = [copy.deepcopy(empty) for i in range(100)] + [copy.deepcopy(grid)] + [copy.deepcopy(empty) for i in range(100)]
 
-grids = []
-while True:
-    ngrid = copy.deepcopy(grid)
-    for y, row in enumerate(grid):
-        for x, tile in enumerate(row):
-            up = check(grid, y, x, 1)
-            down = check(grid, y, x, -1)
-            left = check(grid, y, x, -2)
-            right = check(grid, y, x, 2)
-            num = up+down+left+right
+for i in range(200):
+    ngrids = copy.deepcopy(grids)
+    for d in range(len(grids)):
+        for y, row in enumerate(grids[d]):
+            for x, tile in enumerate(row):
+                up = check(grids, y, x, 1, d)
+                down = check(grids, y, x, -1, d)
+                left = check(grids, y, x, -2, d)
+                right = check(grids, y, x, 2, d)
+                num = up+down+left+right
 
-            if (tile == '.') and (num in [1, 2]):
-                ngrid[y][x] = '#'
+                if (tile == '.') and (num in [1, 2]):
+                    ngrids[d][y][x] = '#'
 
-            if (tile == '#') and (num != 1):
-                ngrid[y][x] = '.'
-    if ngrid in grids:
-        print("HEYPOO")
-        break
+                if (tile == '#') and (num != 1):
+                    ngrids[d][y][x] = '.'
+    grids = ngrids
 
-    grids.append(ngrid)
-    grid = ngrid
-
-bioscore = 0
-for y, row in enumerate(ngrid):
-    for x, tile in enumerate(row):
-        if tile == '#':
-            bioscore += 2**((y*len(row)) + x)
-print(bioscore)
+bugs = 0
+for grid in grids:
+    for row in grid:
+        for tile in row:
+            if tile == '#':
+                bugs += 1
+print(bugs)
