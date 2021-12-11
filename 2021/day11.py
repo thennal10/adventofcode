993179ba3bb32a86d07541fef30403588b60782f
thennal <thennal10@gmail.com>
2021-12-11 11:53:11 +0530

    slight refactor

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,15 +1,14 @@
 import numpy as np
 input = np.array([[int(i) for i in l if i != '\n'] for l in open('./input.txt').readlines()])
 
-def get_adjacent(i, j):
-    adj = []
+def flash(i, j):
+    input[i][j] = -10**3
     for dx in range(-1, 2):
         for dy in range(-1, 2):
             r = range(0, 10)
             (x, y) = (i+dx, j+dy)
             if (x in r) and (y in r) and (dx, dy) != (0, 0):
-                adj.append((x, y))
-    return adj
+                input[x][y] += 1
 
 p1 = 0
 p2 = None
@@ -19,9 +18,7 @@ while not p2:
     input = input + 1
     while (input > 9).any():
         for i, j in np.transpose((input > 9).nonzero()):
-            input[i][j] = -10**3
-            for x, y in get_adjacent(i, j):
-                input[x][y] += 1
+            flash(i, j)
     
     if (input < 0).all(): 
         p2 = step
@@ -30,7 +27,6 @@ while not p2:
         p1 += (input < 0).sum()
     
     input[input < 0] = 0
-
 
 print(p1)
 print(p2)
